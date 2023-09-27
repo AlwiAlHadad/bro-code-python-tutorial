a4932495044ea667570b8b169632400ab7ff840d
Alwi Al Hadad <alwialhaddad00@gmail.com>
2023-09-27 17:42:23 +0700

    lanjooott

---
+++ module40/car.py
@@ -0,0 +1,13 @@
+class Car: # good practice class menggunakan kapital
+
+    def __init__(self, make, model, year, color):
+        self.make = make    #instance variable
+        self.model = model  #instance variable
+        self.year = year    #instance variable
+        self.color = color  #instance variable
+
+    def drive(self):
+        print(f'This {self.model} is driving')
+
+    def stop(self):
+        print(f'This {self.model} is stoooopeedd')
